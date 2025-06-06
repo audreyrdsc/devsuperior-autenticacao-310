e931727386852ad38056334777446e799eed5c2a
audreyrdsc <audreyrdsc@gmail.com>
2025-06-06 14:32:37 -0300

    Inclusão do BCryptPasswordEncoder para criptografar a senha

--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,10 +3,19 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+//Define configurações através de métodos
 @Configuration
 public class SecurityConfig {
+
+    //Criptografia da senha
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean  //metodo para controle de acesso global
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {

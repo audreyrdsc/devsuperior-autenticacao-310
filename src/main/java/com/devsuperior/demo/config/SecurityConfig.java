a86d7bfa5d3278ee7001ac7ba4c0838189c6b1a0
audreyrdsc <audreyrdsc@gmail.com>
2025-06-06 13:43:40 -0300

    Atualização do SecurityConfig

--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.devsuperior.demo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean  //metodo para controle de acesso global
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()); //desabilita proteção contra ataques csrf - aplicação que grava dados na sessão, diferente de API que não guarda
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); //configura permissão para as requisições - endpoints
+        return http.build();
+    }
+
 }
